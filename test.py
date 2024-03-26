3ca3ea7f8e6ea259cae5f8cd68b6a1a7a6fed872
Dominik Arment <domarment@gmail.com>
2024-03-26 11:14:43 -0400

    pymongo

--- test.py
+++ test.py
@@ -1,1 +1,19 @@
-test = 12
+import pymongo
+
+# Connect to MongoDB server
+client = pymongo.MongoClient("mongodb://localhost:27017/")
+
+# Select database (create it if it doesn't exist)
+db = client["demo"]
+
+# Select collection (create it if it doesn't exist)
+collection = db["names"]
+
+# Data to insert
+data = {"name": "John", "age": 30, "city": "New York"}
+
+# Insert data into collection
+result = collection.insert_one(data)
+
+# Print the inserted document's ID
+print("Inserted ID:", result.inserted_id)
